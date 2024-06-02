3837e70b968275ca66c569103e92a62c8e260204
00xZ <eyezikbra@gmail.com>
2024-06-02 02:23:17 -0500

    Update SeeAll.sh
    it has to sleep im sorry

--- discover/SeeAll.sh
+++ discover/SeeAll.sh
@@ -1,15 +1,17 @@
+#!/bin/bash
 while true
 do
-	echo " Scanning For Random Servers "
-	zmap -N 5 -p 443 -o ips.txt
-	echo " [+] Pulling URLs From IP [+] "
-	gdn ips.txt | awk '{print $2}' | sort -u > target.txt
-		echo " [+] Grabbing info[+] "
+	zmap -N 7 -p 443 -o ips.txt -B 100M
+	echo " 10 Sec Sleep... yes its needed "
+	sleep 10
+	cat ips.txt | gdn |awk '{print $2}' | tee targets.txt
+	cat targets.txt | sort -u | uniq | tee target.txt
+	echo " [+] Grabbing info[+] "
+	cat target.txt | anew raw_db.txt
 	cat target.txt | httpx -sc -title | anew database.txt
-		echo " [+] Parsing [+] "
+	echo " [+] Parsing [+] "
 	cat database.txt | grep "200" | awk '{print $1}' | anew all.txt
 	rm ips.txt
 	rm target.txt
 	echo " [!] LOOP [!] "
-	killall gdn
 done
