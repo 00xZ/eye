64c8d8f704c7ee0bbcbdcc4659007493ef962147
00xZ <eyezikbra@gmail.com>
2024-06-15 00:09:40 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -96,6 +96,110 @@ cd httpx/cmd/httpx
 go build
 mv httpx /bin/
 cd ~/eye
+########## ffuf
+
+
+
+echo "${cyan}Installing ffuf... ${reset}"
+cd tools
+git clone https://github.com/ffuf/ffuf.git
+echo -e "Building the main.go file.\n${yellow}[Warning] This process fails if you don't have go installed. If you have any error messages below, try again after installing go.${reset}"
+cd ffuf
+go build main.go help.go
+chmod +x main
+cd ../..
+
+
+
+########## OpenRedireX
+
+
+
+
+echo "${cyan}Installing OpenRedireX... ${reset}"
+cd tools 
+git clone https://github.com/devanshbatham/OpenRedireX.git
+cd ..
+echo -e "openredirex(){
+	cat $1 | python3 tools/OpenRedireX/openredirex.py -p \$2
+}" >> .profile
+echo -e "${green}Done${reset}\n"
+
+
+
+
+########## qsreplace
+
+
+
+
+
+echo "${cyan}Installing qsreplace...${reset}"
+cd tools
+git clone https://github.com/tomnomnom/qsreplace
+echo -e "Building the main.go file.\n${yellow}[Warning] This process fails if you don't have go installed. If you have any error messages below, try again after installing go.${reset}"
+cd qsreplace
+go build main.go
+cd ../..
+
+cp tools/qsreplace/main /bin/qsreplace
+
+
+
+
+########## Gxss
+
+
+
+
+git clone https://github.com/KathanP19/Gxss
+cd Gxss
+echo "${green}Installing Gxss${reset}"
+go build
+cp tools/Gxss /bin/Gxss
+echo "${cyan}Gxss Installed ${reset}"
+cd ..
+
+########## Dalfox
+
+
+git clone https://github.com/hahwul/dalfox
+cd dalfox
+go build
+echo "${green}Installing DalFox${reset}"
+cp dalfox /bin/dalfox
+cd ..
+echo "${cyan}Installed DalFox${reset}"
+
+########## waymore
+
+
+python3 -m pip install waymore
+
+########## ghauri
+cd tools
+git clone https://github.com/r0oth3x49/ghauri.git
+cd ghauri
+python3 -m pip install --upgrade -r requirements.txt
+python3 setup.py install
+cd ..
+############### Xray
+wget https://github.com/chaitin/xray/releases/download/1.9.11/xray_linux_amd64.zip
+unzip xray_linux_amd64.zip
+echo "xray installed"
+
+########## gf and its patterns
+git clone https://github.com/tomnomnom/gf.git
+cd gf
+go build
+cp gf /bin/
+cd ..
+mkdir ~/.gf
+git clone https://github.com/1ndianl33t/Gf-Patterns.git
+cp Gf-Patterns/*.json ~/.gf/
+echo "${red} "
+gf -list
+######### LFIscanner
 cd tools
 git clone https://github.com/R3LI4NT/LFIscanner.git
 cd LFIscanner
